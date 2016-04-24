a3d93548d77c395071b2dc98d05adfa4488ceee5
User <toireasam@hoxton.home>
2016-04-24 13:48:28 +0100

    Initial Commit

---
+++ bAdventurousTourist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bAdventurousTourist
+//
+//  Created by Toireasa Moley on 24/04/2016.
+//  Copyright © 2016 Toireasa Moley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
